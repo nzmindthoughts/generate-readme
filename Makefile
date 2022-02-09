8338c23e3fd8c72b2192e6f6a794efad6ed53413
tsamuel33 <tsamuel86@gmail.com>
2022-02-08 23:47:56 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+.PHONY: test clean deploy
+
+all:
+	@echo "ERROR: noop"
+	@exit 1
+
+test:
+	@python3 -m unittest discover
+	@echo "OK: $@"
+
+deploy:
+	@python3 ./deploy/deploy.py --account_number $(ACCOUNT_NUMBER) --aws_environment $(ENVIRONMENT_NAME)
+	@echo "OK: $@"
+
+generate_readme:
+	@python3 ./generate_readme/script.py --aws_environment $(ENVIRONMENT_NAME)
+	@echo "OK: $@"
+
+clean:
+	@for f in `find . -name '*.pyc'`;do rm -rf $$f; done
+	@echo "OK: $@"
---
+++ generate_readme/script.py
@@ -0,0 +1,46 @@
+# from jinja2 import Environment, FileSystemLoader
+from jinja2 import Template
+import argparse
+import json
+from pathlib import Path
+
+# parser = argparse.ArgumentParser(description='Accept application environment')
+# parser.add_argument('--aws_environment', type=str, help='Environment of AWS account in which templates will be deployed', required=True)
+# args = vars(parser.parse_args())
+
+# ENVIRONMENT_NAME = args['aws_environment']
+ENVIRONMENT_NAME = 'prod'
+GENERATE_DIR = Path(__file__).parents[0]
+ROOT_DIR = Path(GENERATE_DIR).parents[0]
+JINJA_TEMPLATE_DIR = ROOT_DIR / 'jinjastuff'
+CONFIG_DIR = ROOT_DIR / 'config'
+CONFIG_FILE_NAME = 'account_info.'+ENVIRONMENT_NAME+'.json'
+
+def load_file(file):
+    with open(file, 'r') as myfile:
+        data = myfile.read()
+    return data
+
+def get_render_variables(file):
+    output = {}
+    filedata = load_file(file)
+    obj = json.loads(filedata)
+    output['alias'] = obj['AccountAlias']
+    output['env'] = obj['ApplicationEnvironment']
+    output['group'] = obj['ApplicationGroup']
+    output['Id'] = obj['ApplicationId']
+    output['name'] = obj['ApplicationName']
+    output['primary'] = obj['ApplicationPrimaryStakeholder']
+    output['secondary'] = obj['ApplicationSecondaryStakeholder']
+    return output
+
+def main():
+    templatesource = load_file(str(JINJA_TEMPLATE_DIR)+"/README.j2")
+    template = Template(source=templatesource)
+    template_variables = get_render_variables(str(CONFIG_DIR)+"/"+str(CONFIG_FILE_NAME))
+    readme_contents = template.render(template_variables)
+    with open(str(ROOT_DIR)+"/README.md", "w") as readme:
+        readme.write(readme_contents)
+    
+if __name__ == "__main__":
+    main()
